82def7828d6432b09ce06e357d1765db9c7eddf0
rob-schlenker <schlenk7@gmail.com>
2025-02-11 21:49:24 -0500

    refactor chat component for improved layout and user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,16 +25,21 @@ export default function Chat() {
   })
 
   return (
-    <div className="flex flex-col w-full max-w-2xl py-24 mx-auto stretch h-screen">
-      <div className="text-center text-4xl mb-12">
+    <div className="flex flex-col w-full h-screen max-h-dvh">
+      <div className=" text-center text-4xl my-12 max-w-3xl mx-auto p-4">
         <h1 className={roboto.className}>Rob's A.I. Chatbot</h1>
       </div>
-      <div className='h-screen mx-4'>
+      <div className="mx-auto container p-4 flex flex-col flex-grow max-w-3xl">
         <ul
           ref={chatParent}
-          className="h-full p-4 flex-grow bg-muted/50 rounded-lg overflow-y-auto flex flex-col gap-4 bg-white border-gray-400 border-1 shadow-m"
+          className="h-2 p-4 flex-grow bg-muted/50 rounded-lg overflow-y-auto flex flex-col gap-4 bg-white border-gray-400 border-1 shadow-m"
         >
-          {messages.map((m, index) =>
+          {messages.length === 0 ? (
+    <li className="text-gray-500 text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      Please ask a question below...
+    </li>
+  ) : (
+          messages.map((m, index) =>
             m.role === 'user' ? (
               <li key={`user-${index}`} className="flex flex-row">
                 <div className="rounded-xl p-4 bg-gray-800 shadow-md flex">
@@ -51,17 +56,69 @@ export default function Chat() {
                 </div>
               </li>
             ),
-          )}
+          ))}
+          {/* <p className="text-black">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            enim ut, a eos, distinctio nesciunt quisquam magni, dolore eligendi
+            obcaecati officiis nobis provident voluptates eveniet quis est
+            labore excepturi soluta?
+          </p> */}
         </ul>
 
-        <form onSubmit={handleSubmit}>
-          <input
-            className="dark:bg-zinc-900 bottom-0 max-w-2xl p-2 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl w-full mt-4"
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </form>
+        <section className="py-4">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="dark:bg-zinc-900 p-4 bottom-0 max-w-3xl mx-auto border border-zinc-300 dark:border-zinc-800 rounded shadow-xl w-full"
+              value={input}
+              placeholder="Say something..."
+              onChange={handleInputChange}
+            />
+          </form>
+        </section>
       </div>
     </div>
   )
